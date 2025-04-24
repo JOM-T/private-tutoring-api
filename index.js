7c43110f04949bb1125b70518b2565d1f3832fa5
Matthew Dinko <matthewdinko8@gmail.com>
2025-04-24 16:56:18 +0000

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import 'dotenv/config'
 import teacherRouter from "./routes/teachers.js";
 import userRouter from "./routes/regUser.js";
@@ -13,6 +14,7 @@ await mongoose.connect(process.env.MONGO_URI);
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(teacherRouter);
 app.use(userRouter);
 app.use(applicationRouter);
